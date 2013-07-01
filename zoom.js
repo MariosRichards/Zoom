debfa24f995c7c0afc19fc8b252df4670d68a569
Marios Richards <Marios.Richards@gmail.com>
2013-07-01 13:12:20 +0200

    Stage 2 complete!

--- zoom.js
+++ zoom.js
@@ -14,8 +14,12 @@ this.translationAccY = 0;
 this.init_wheel = function()
 	{
 
-	this.centreX = $(self.div).width()/2 + $(self.div).position().left;
-	this.centreY = $(self.div).height()/2 + $(self.div).position().top;
+	this.centreX = $(self.div).width()/2;
+	this.centreY = $(self.div).height()/2;
+	
+	this.posLeft = $(self.div).position().left;
+	this.posTop = $(self.div).position().top;
+	
 	//alert(this.centreX +','+this.centreY);	
 	
 	
@@ -26,7 +30,7 @@ this.init_wheel = function()
 		+"$(self.div).position().left = "+$(self.div).position().left+","+"$(self.div).position().top = "+$(self.div).position().top
 		+"self.translationAccX = "+self.translationAccX+","+"this.translationAccY = "+self.translationAccY
 		);
-	//	$("#info").append().text("mouseX = "+event.clientX+","+"mouseY = "+event.clientY);
+
 		
 		
 
@@ -77,9 +81,33 @@ this.init_wheel = function()
 
 		// alert(self.zoom);	
 		
+		
+		
+		// Z vector is in the coordinate system
+		// of the original image
+		// (obviously, when you zoom around one point
+		// it remains where it was in the original image!)
+		
+		// more generally (i.e. if you move the cursor between zooms)
+		// we need to translate from the screen pixel to
+		// the original image pixel position
+		
+		// x in original image pixels
+		// y in original image pixels
+		// x->x'     :     (x-self.centreX) + self.centreX + self.posLeft                (x=0 -> x'= self.posLeft)
+		// x'->x''   :     (x-self.centreX) * self.zoom + self.centreX + self.posLeft  
+		// x''->x''' : y = (x-self.centreX) * self.zoom + self.centreX + self.posLeft + self.translationAccX
+		
+		// ((y - self.translationAccX - self.centreX - self.posLeft)/self.zoom) + self.centreX
+		// y = self.posLeft; self.translationAccX = 0; self.zoom=1 -> x = 0
+		
+		// add a final self.posLeft to get to original screen pixels from original image pixels
+		
+		x = ((x - self.translationAccX - self.centreX - self.posLeft)/self.zoom) + self.centreX + self.posLeft;
+		y = ((y - self.translationAccY - self.centreY - self.posTop)/self.zoom) + self.centreY + self.posTop;
 
-		var Zx = (x - self.centreX); // 400
-		var Zy = (y - self.centreY); // 200
+		var Zx = (x - self.centreX - self.posLeft); // 400
+		var Zy = (y - self.centreY - self.posTop); // 200
 	//	alert(Zx +','+ Zy);
 		
 	//	alert("x, " + x + " y: " + y);		
